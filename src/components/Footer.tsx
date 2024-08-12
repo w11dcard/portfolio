463382a21d8e4e19f8dc422e317f72e47034ef30
matheus <matheus.felipe.19rt@gmail.com>
2024-08-12 16:49:56 -0300

    style: Update Footer component styles and layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,29 @@
+import { Icon } from "@iconify/react"
+import logo from "/logo.png"
+
 export default function Footer() {
 	return (
-		<footer className="p-4">
-			<div className="container mx-auto text-center">
-				<p></p>
+		<footer id="footer" className="card-shadow footer-container">
+			<img src={logo} alt="Logo" className="icon" />
+
+			<div className="m-6 flex justify-center gap-4 text-secondary">
+				<a href="https://github.com/w11dcard">
+					<Icon icon="simple-icons:github" className="icon h-6 w-6" />
+				</a>
+				<a href="https://www.linkedin.com/in/matheus-mortari-19rt/">
+					<Icon icon="simple-icons:linkedin" className="icon h-6 w-6" />
+				</a>
+				<a href="mailto:matheus.felipe.19rt@gmail.com">
+					<Icon icon="simple-icons:gmail" className="icon h-6 w-6" />
+				</a>
+			</div>
+
+			<div className="text-center text-xs">
+				Icons provided by{" "}
+				<a href="https://iconify.design/" className="text-secondary hover:underline">
+					Iconify Design.
+				</a>
+				<p>© 2024 Matheus Mortari. All rights reserved.</p>
 			</div>
 		</footer>
 	)
