8bd1ea3811993b98951a95273d902b582b76553d
matheus <matheus.felipe.19rt@gmail.com>
2024-08-31 15:19:08 -0300

    chore: Install & use Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import About from "./components/About"
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
@@ -8,6 +9,7 @@ import TopNav from "./components/TopNav"
 export default function App() {
 	return (
 		<div className="flex min-h-screen flex-col">
+			<Analytics />
 			<TopNav />
 			<Hero />
 
